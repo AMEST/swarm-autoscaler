4503d15ca0478d83c8d753e12e5f52987bfb0367
Клабуков Эрик Константинович <klabukov@skbkontur.ru>
2022-01-09 21:40:44 +0500

    add thread pool for http client

--- src/discovery.py
+++ src/discovery.py
@@ -4,11 +4,12 @@ import socket
 from requests import get
 import json
 from cache import Cache
-
+from multiprocessing.dummy import Pool as ThreadPool
 
 class Discovery(object):
     DiscoveryCacheKey = "discovery_hosts"
     def __init__(self, discoveryDnsName, memoryCache: Cache, checkInterval: int):
+        self.threadPool = ThreadPool(8)
         self.cache = memoryCache
         self.cacheTime = checkInterval / 2
         self.discoveryName = discoveryDnsName
@@ -21,10 +22,18 @@ class Discovery(object):
                          (containerId, cpuLimit))
 
     def __sendToAll(self, url):
-        for ip in self.__getClusterHosts():
-            result = get("http://%s%s" %(ip,url))
-            if(result != None and result.status_code == 200):
-                return json.loads(result.text)
+        hosts = self.__getClusterHosts()
+        requests = list("http://%s%s" %(ip,url) for ip in hosts)
+        results = self.threadPool.map(self.__send, requests)
+        for result in results:
+            if(result != None):
+                return result
+        return None
+
+    def __send(self, url):
+        result = get(url)
+        if(result != None and result.status_code == 200):
+            return json.loads(result.text)
         return None
 
     def __getClusterHosts(self):
